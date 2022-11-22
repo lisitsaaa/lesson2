7c753fb01253efec46cf96e8dc60b9d244cd61f0
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2022-11-22 23:53:47 +0300

    add all files

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        for(int i = 0; i<=9 ; i++){
+            System.out.print(i + " - You need to write down your name: ");
+            String name = input.nextLine();
+
+            System.out.println("Hello " + name);
+            System.out.println("-*-*-*-*-*-*--*-*-*--*-*-*--*-*-*--*-*-*");
+        }
+    }
+}
