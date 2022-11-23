39f106c7b4fae9edaba38112fd1016d0a29b460f
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2022-11-23 20:09:00 +0300

    add package out in gitignore

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        for(int i = 0; i<=9 ; i++){
+
+        for (int i = 0; i <= 9; i++) {
             System.out.print(i + " - You need to write down your name: ");
             String name = input.nextLine();
 
